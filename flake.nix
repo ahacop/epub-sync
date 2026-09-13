6e23e1f3c9951d21fe71ca125e92479bae80a750
Ara Hacopian <ara@hacopian.de>
2026-09-13 19:37:24 +0200

    Release 0.1.2

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "epubsync";
-            version = "0.1.1";
+            version = "0.1.2";
             src = fs.toSource {
               root = ./.;
               fileset = fs.unions [ ./Cargo.toml ./Cargo.lock ./crates ./kepub-shim ];
